085b811c3b07f79b065f6bb7b50d9ef452be53c5
Popularkiya <kingawladzinsk@gmail.com>
2022-12-01 11:48:05 +0100

    data generation script + mongodb adjustments

--- init-mongodb/initDb.js
+++ init-mongodb/initDb.js
@@ -1,24 +1,23 @@
 //sanity check
 print("################################################INITIALIZING DATABASE########################################")
 
-// Create user
-dbAdmin = db.getSiblingDB("admin");
-dbAdmin.createUser({
-  user: "root",
-  pwd: "student",
-  roles: [{ role: "userAdminAnyDatabase", db: "admin" }],
-});
-
-// Authenticate user
-dbAdmin.auth({
-  user: "root",
-  pwd: "student",
-  digestPassword: true,
-});
-
 // Create DB and collection
-db = new Mongo().getDB("SI_175237");
-db.createCollection("Temperature", { capped: false });
-db.createCollection("Humidity", { capped: false });
-db.createCollection("Ultraviolet", { capped: false });
-db.createCollection("CarbonDioxide", { capped: false });
+db = db.getSiblingDB('SI_175237')
+
+db.createUser({
+  user: 'root',
+  pwd: 'student',
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'SI_175237',
+    },
+  ],
+});
+
+db.createCollection('Temperature', { capped: false });
+db.createCollection('Humidity', { capped: false });
+db.createCollection('Ultraviolet', { capped: false });
+db.createCollection('CarbonDioxide', { capped: false });
+
+print("################################################END OF THE INITIALIZATION########################################")
